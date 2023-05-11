dadb48cfc1a614b9d8963d67f53faf62784a59f2
Maud <maud.leleux@metrum.lu>
2023-05-11 15:31:08 +0200

    [ADD]first_owl_view

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "First Owl view",
+    "summary": "Tutorial about Creating an OWL View from scratch.",
+    "author": "LittleOdoo based on Coding Dodo",
+    "category": "Tools",
+    "version": "14.0.0",
+    "depends": ["base", "web", "mail", "product"],
+    "data": [],
+    
+}
